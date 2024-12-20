d11343aed89b8f194d8b81fcfa2c1730ea8aa637
Hammad Wasi <hammadwasi.b@gmail.com>
2024-12-20 15:07:03 +0500

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use core::error;
+
 fn main() {
     let file = "my.txt".to_owned();
     let data = read_file(&file);
@@ -10,4 +12,10 @@ fn read_file(file_name: &str) -> Option<String> {
     std::fs::read_to_string(file_name)
         .ok()
         .and_then(|s| s.lines().next().map(|l| l.to_owned()))
+}
+
+fn parse_file(file_name: &str) -> Result<i32, Box<dyn error::Error>> {
+    let file = std::fs::read_to_string(file_name)?;
+    let i = file.parse()?;
+    Ok(i)
 }
